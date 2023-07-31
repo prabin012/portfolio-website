e18aa554ccb9dbcd9e77d8c181c3f4d9bfe03da5
Prabin Kumar Mahato <prabinmahato414@gmail.com>
2023-07-31 14:27:05 +0545

    first commit

--- index.js
+++ index.js
@@ -30,9 +30,64 @@ sunn.onclick = function (){
     
     }
 }
+
+let slideIndex=0;
+function slideShow(){
+    try{
+        let i;
+        let blogss=document.getElementsByClassName("blog-box");
+        for(i=0; i< blogss.length; i++){
+            blogss[i].style.display="none";
+        }
+        slideIndex ++;
+    
+        if(slideIndex > blogss.length){
+            slideIndex =1;
+        }
+        blogss[slideIndex-1].style.display="block";
+        setTimeout(slideShow, 3000);
+        
+    }
+    catch{
+        console.error();
+    }
+    
+}
+slideShow();
+  
+let slideIndex1=0;
+function slideShows(){
+    try{
+        let i;
+        let blogss=document.getElementsByClassName("blog-box-1");
+        for(i=0; i< blogss.length; i++){
+            blogss[i].style.display="none";
+        }
+        slideIndex1 ++;
+    
+        if(slideIndex1 > blogss.length){
+            slideIndex1 =1;
+        }
+        blogss[slideIndex1-1].style.display="block";
+        setTimeout(slideShows, 3000);
+       
+    }
+    catch{
+        console.error();
+    }
+    
+}
+slideShows();
+
+
+
+
     
 
     // }
+
+
+
 
 
 
